68d5919f992613ae0aebedc7417068b67c2cdcd3
au141968 <austinrobertsdesign@gmail.com>
2026-07-22 18:44:09 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-const button = document.getElementById('glowButton');
+const buttons = document.querySelectorAll('.imageBtn');
+const popups = document.querySelectorAll('.textPopup');
+const closeBtns = document.querySelectorAll('.closeBtn');
 
-button.addEventListener('click' , () => {
-    button.classList.toggle('active');
+buttons.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        popups[index].classList.add('show');
+    });
+});
+
+closeBtns.forEach((close, index) => {
+    close.addEventListener('click', () => {
+        popups[index].classList.remove('show');
+    });
+});
+
+window.addEventListener('click', (event) => {
+    popups.forEach(popup => {
+        if (event.target === popup) {
+            popup.classList.remove('show');
+        }
+    });
 });
